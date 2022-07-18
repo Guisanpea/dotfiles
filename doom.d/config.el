5b27657abfd3bf31c84c0a90f44c367b0448bb97
Santi <guisanpea@gmail.com>
2022-07-18 17:28:54 +0200

    Changed small things

--- doom.d/config.el
+++ doom.d/config.el
@@ -28,11 +28,11 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-spacegrey)
+(setq doom-theme 'doom-nord-light)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Dropbox/org")
 
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
